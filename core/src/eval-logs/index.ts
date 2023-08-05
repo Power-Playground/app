8345d3f62b86afd323f6a5a1a091bb38ca2d2a8f
yijie4188 <yijie4188@gmail.com>
2023-08-05 16:11:44 +0800

    fix: remove useless import, wait update

--- core/src/eval-logs/index.ts
+++ core/src/eval-logs/index.ts
@@ -1,32 +1,17 @@
-import * as Awaitabler from 'awaitabler'
-import * as AwaitablerString from 'awaitabler/prototypes/string.reg'
-import * as AwaitablerNumber from 'awaitabler/prototypes/number.reg'
-
 import { elBridgeC } from './bridge.ts'
 import { Files } from './files.ts'
 
 // @ts-ignore
 window.require = function (name) {
-  switch (name) {
-    case 'awaitabler': return Awaitabler
-    case 'awaitabler/prototypes/number': {
-      addDisposeFunc(AwaitablerNumber.default())
-      return
-    }
-    case 'awaitabler/prototypes/number.reg': return AwaitablerNumber
-    case 'awaitabler/prototypes/string': {
-      addDisposeFunc(AwaitablerString.default())
-      return
-    }
-    case 'awaitabler/prototypes/string.reg': return AwaitablerString
-  }
+  // switch (name) {
+  // }
   throw new Error(`Cannot find module '${name}'`)
 }
 
 let prevDisposeFunc: Function
 
 function addDisposeFunc(func?: Function) {
-  let oldDisposeFunc = prevDisposeFunc
+  const oldDisposeFunc = prevDisposeFunc
   prevDisposeFunc = () => {
     oldDisposeFunc?.()
     func?.()
