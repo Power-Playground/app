a9be462bd6c6dd7b8ee31cff3e603137fed86516
yijie4188 <yijie4188@gmail.com>
2023-08-10 02:16:43 +0800

    fix(core): disable useless lint error for isMacOS polyfill

--- core/src/utils/isMacOS.ts
+++ core/src/utils/isMacOS.ts
@@ -1,3 +1,5 @@
-var navigator: navigaor
+// noinspection ES6ConvertVarToLetConst
+// eslint-disable-next-line no-var
+var navigator: any
 
 export const isMacOS = navigator?.platform?.startsWith('Mac')
