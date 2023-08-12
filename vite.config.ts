de95a8520ab406e43daab26c451de53e498d23c7
kanno <812137533@qq.com>
2023-08-12 10:24:14 +0800

    chore: migrate external plugin to cdn plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,21 +2,25 @@ import react from '@vitejs/plugin-react'
 import { configDotenv } from 'dotenv'
 import path from 'node:path'
 import { defineConfig } from 'vite'
-import { viteExternalsPlugin } from 'vite-plugin-externals'
+import { cdn } from 'vite-plugin-cdn2'
+import { unpkg } from 'vite-plugin-cdn2/url.js'
 
 configDotenv()
 
 // https://vitejs.dev/config/
-export default defineConfig(async env => ({
+export default defineConfig(async _=> ({
   base: `/${process.env.BASE_URL || 'app'}/`,
   plugins: [
     react(),
-    env.mode === 'production' ? viteExternalsPlugin({
-      '@babel/standalone': 'Babel',
-      'react': 'React',
-      'react-dom': 'ReactDOM',
-      'sentinel-js': 'sentinel'
-    }) : null
+    cdn({
+      url:unpkg,
+      modules:[
+        { name:'sentinel-js', relativeModule:'./dist/sentinel.umd.js' },
+        { name:'@babel/standalone', relativeModule:'./babel.min.js' },
+        { name: 'react', relativeModule:'./umd/react.production.min.js' },
+        { name:'react-dom', relativeModule:'./umd/react-dom.production.min.js' }
+      ]
+    })
   ],
   publicDir: './core/public',
   build: {
