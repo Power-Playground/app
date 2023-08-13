ad17200b89cd285cd58ab6ae04f313e3863495e5
yijie4188 <yijie4188@gmail.com>
2023-08-13 15:53:57 +0800

    feat(core): collect plugin effect

--- src/plugins/remove-left-toolbar-items.ts
+++ src/plugins/remove-left-toolbar-items.ts
@@ -2,22 +2,28 @@ import { definePlugin } from '@power-playground/core'
 
 export default definePlugin({
   devtools: {
-    async beforeMount({ devtoolsWindow }) {
+    beforeMount({ devtoolsWindow }) {
       const { document } = devtoolsWindow
       const style = document.createElement('style')
       style.innerText = `.tabbed-pane-header > .tabbed-pane-left-toolbar.toolbar { display: none !important; }`
+      let tabbedPane: Element | null
       // TODO refactor by sentinel, now it's a hack
       //      unable load right document when use sentinel
       //      so I think sentinel which need to fork
-      // eslint-disable-next-line no-constant-condition
-      while (true) {
-        const tabbedPane = document.querySelector('.main-tabbed-pane')
-        if (!tabbedPane) {
-          await new Promise(resolve => setTimeout(resolve, 100))
-          continue
+      !async function() {
+        // eslint-disable-next-line no-constant-condition
+        while (true) {
+          tabbedPane = document.querySelector('.main-tabbed-pane')
+          if (!tabbedPane) {
+            await new Promise(resolve => setTimeout(resolve, 100))
+            continue
+          }
+          tabbedPane.shadowRoot?.appendChild(style)
+          break
         }
-        tabbedPane.shadowRoot?.appendChild(style)
-        break
+      }()
+      return () => {
+        tabbedPane?.shadowRoot?.removeChild(style)
       }
     },
     load({ inspectorView: { tabbedPane } }) {
