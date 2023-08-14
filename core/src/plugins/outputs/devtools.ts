52a0258b14d0eb3c663bd38872e42f1d3c0733b4
yijie4188 <yijie4188@gmail.com>
2023-08-14 14:51:46 +0800

    refactor(core): register on event in plugin load hook

--- core/src/plugins/outputs/devtools.ts
+++ core/src/plugins/outputs/devtools.ts
@@ -1,9 +1,11 @@
 import './devtools-init'
 
-import { defineDevtools, defineDevtoolsPanel } from '@power-playground/core'
+import * as Babel from '@babel/standalone'
+import { defineDevtools, defineDevtoolsPanel, elBridgeC } from '@power-playground/core'
 
 import { JSPanel } from './panels/javascript'
 import { DTSPanel } from './panels/typescript'
+import { Files, setFiles } from './files'
 
 // TODO More Panel
 //   Errors
@@ -12,5 +14,61 @@ export default defineDevtools({
   panels: [
     defineDevtoolsPanel('outputs.js', '.JS', 'react', JSPanel),
     defineDevtoolsPanel('outputs.d.ts', '.D.TS', 'react', DTSPanel)
-  ]
+  ],
+  load() {
+    let prevDisposeFunc: Function
+    function addDisposeFunc(func?: Function) {
+      const oldDisposeFunc = prevDisposeFunc
+      prevDisposeFunc = () => {
+        oldDisposeFunc?.()
+        func?.()
+      }
+    }
+    const disposeRun = elBridgeC.on('run', () => {
+      Files.forEach(({ name, text: code }) => {
+        if (name !== '/index.js') return
+
+        // TODO support fileSystem
+        try {
+          prevDisposeFunc?.()
+          addDisposeFunc(eval(
+            `(function () { const module = { exports: {} }; const exports = module.exports; ${code}; return module.exports; })()`
+          ).dispose)
+        } catch (e) {
+          console.error(e)
+        }
+      })
+    })
+
+    // TODO resolve babel plugins management
+    const disposeCompileWatch = elBridgeC.on('compile-completed', files => {
+      setFiles(files.map(({ name, text }) => {
+        let code = text
+        if (name.endsWith('.js')) {
+          name = name.slice(7)
+          try {
+            code = Babel.transform(text, {
+              presets: ['es2015'],
+              plugins: [
+              ],
+              filename: name
+            })?.code ?? ''
+          } catch (e) {
+            return {
+              name: `${name} (compile error)`,
+              originalText: text,
+              // @ts-ignore
+              text: e!.message!
+            }
+          }
+        }
+        return { name, originalText: text, text: code ?? '' }
+      }))
+    })
+    return () => {
+      disposeRun?.()
+      disposeCompileWatch?.()
+      prevDisposeFunc?.()
+    }
+  }
 })
--- core/src/plugins/outputs/files.ts
+++ core/src/plugins/outputs/files.ts
@@ -1,13 +1,16 @@
 import { useSyncExternalStore } from 'react'
-import * as Babel from '@babel/standalone'
-
-import type { EvalLogsIframeParentEvent } from '../../eval-logs/bridge'
-import { elBridgeC } from '../../eval-logs/bridge'
+import type { EvalLogsIframeParentEvent } from '@power-playground/core'
+import { elBridgeC } from '@power-playground/core'
 
 export let Files: (
   & Extract<EvalLogsIframeParentEvent, { type: 'compile-completed' }>['data'][number]
   & { originalText: string }
 )[] = []
+
+export function setFiles(files: typeof Files) {
+  Files = files
+  listeners.forEach(listener => listener(Files))
+}
 
 type Listener = (files: typeof Files) => void | Promise<void>
 const listeners: Listener[] = []
@@ -22,31 +25,3 @@ const getFilesSubscribe = (callback: Listener) => {
 export function useFiles() {
   return useSyncExternalStore<typeof Files>(getFilesSubscribe, () => Files)
 }
-
-// TODO resolve babel plugins management
-
-elBridgeC.on('compile-completed', files => {
-  Files = files.map(({ name, text }) => {
-    let code = text
-    if (name.endsWith('.js')) {
-      name = name.slice(7)
-      try {
-        code = Babel.transform(text, {
-          presets: ['es2015'],
-          plugins: [
-          ],
-          filename: name
-        })?.code ?? ''
-      } catch (e) {
-        return {
-          name: `${name} (compile error)`,
-          originalText: text,
-          // @ts-ignore
-          text: e!.message!
-        }
-      }
-    }
-    return { name, originalText: text, text: code ?? '' }
-  })
-  listeners.forEach(func => func(Files))
-})
