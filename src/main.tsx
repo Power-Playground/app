bb03db68e273b46018cb3a1ba8b33219270a1ba9
yijie4188 <yijie4188@gmail.com>
2023-08-14 15:46:52 +0800

    chore: ㊙️

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,5 @@
+// noinspection JSNonASCIINames
+
 import './main.scss'
 
 import React from 'react'
@@ -6,7 +8,7 @@ import ReactDOM from 'react-dom/client'
 import { App } from './App.tsx'
 
 window.__DEBUG__ = JSON.parse(localStorage.getItem('enableDebug') ?? 'false')
-Object.defineProperty(window, '牛逼', {
+Object.defineProperty(window, '小黑子', {
   get() {
     if (__DEBUG__) {
       __DEBUG__ = false
@@ -16,7 +18,18 @@ Object.defineProperty(window, '牛逼', {
       localStorage.setItem('enableDebug', 'true')
     }
     // debug 模式启动
-    console.log(`debug 模式${__DEBUG__ ? '启动' : '关闭'}`)
+    console.log(`小黑子模式${__DEBUG__ ? '启动' : '关闭'}`)
+    const decoded = unescape(encodeURIComponent(import.meta.glob('../PP_小黑子.svg', {
+      eager: true,
+      as: 'raw'
+    })['../PP_小黑子.svg']))
+    const base64 = btoa(decoded)
+    const imgSource = `data:image/svg+xml;base64,${base64}`
+
+    console.log("%c ", `
+      font-size: 128px;
+      background: url("${imgSource}") no-repeat center;
+    `)
     setTimeout(location.reload.bind(location), 500)
   }
 })
