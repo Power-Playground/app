c45d5b6152ce30c018b0b02dfa22e20e9e698f0b
yijie4188 <yijie4188@gmail.com>
2023-08-15 20:46:00 +0800

    style(core): adjust topbar icon color

--- src/App.tsx
+++ src/App.tsx
@@ -95,7 +95,7 @@ export function App() {
           <div
             className='hide-topbar'
             dangerouslySetInnerHTML={{
-              __html: '<svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 -960 960 960"><path fill="var(--fr-cr)" d="M450-160v-371L330-411l-43-43 193-193 193 193-43 43-120-120v371h-60ZM160-597v-143q0-24 18-42t42-18h520q24 0 42 18t18 42v143h-60v-143H220v143h-60Z"/></svg>'
+              __html: '<svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 -960 960 960"><path fill="var(--icon-cr)" d="M450-160v-371L330-411l-43-43 193-193 193 193-43 43-120-120v371h-60ZM160-597v-143q0-24 18-42t42-18h520q24 0 42 18t18 42v143h-60v-143H220v143h-60Z"/></svg>'
             }}
             onClick={() => {
               setDisplayHeader(false)
