5ccfbb68c447fac3c399eb6d39b07ee716320c34
yijie4188 <yijie4188@gmail.com>
2023-08-16 03:44:14 +0800

    feat(core): set default compiler options target field

--- core/src/plugins/typescript/index.ts
+++ core/src/plugins/typescript/index.ts
@@ -25,6 +25,7 @@ const extraModules = Object
     content
   }), [] as { content: string, filePath: string }[])
 let compilerOptions: monacoEditor.languages.typescript.CompilerOptions = {
+  target: 4,
   moduleResolution: 2,
   declaration: true
 }
@@ -38,52 +39,44 @@ export interface TypeScriptPluginX {
   }
 }
 
-let editor: Editor<TypeScriptPluginX>
-if (import.meta.hot?.data['typescript:editor']) {
-  editor = import.meta.hot.data['typescript:editor']
-} else {
-  editor = {
-    use,
-    useShare({
-      curFilePath, language, typescriptVersion
-    }, monaco) {
-      useEffect(() => {
-        if (!monaco || !typescriptVersion) return
+const editor: Editor<TypeScriptPluginX> = {
+  use,
+  useShare({
+    curFilePath, language, typescriptVersion
+  }, monaco) {
+    useEffect(() => {
+      if (!monaco || !typescriptVersion) return
 
-        let defaults: monacoEditor.languages.typescript.LanguageServiceDefaults
-        if (language === 'javascript') {
-          defaults = monaco.languages.typescript.javascriptDefaults
-        } else {
-          defaults = monaco.languages.typescript.typescriptDefaults
-        }
-        extraModules.forEach(({ content, filePath }) => {
-          monaco.editor.createModel(
-            content,
-            language === 'javascript' ? 'javascript' : 'typescript',
-            monaco.Uri.parse(filePath)
-          )
+      let defaults: monacoEditor.languages.typescript.LanguageServiceDefaults
+      if (language === 'javascript') {
+        defaults = monaco.languages.typescript.javascriptDefaults
+      } else {
+        defaults = monaco.languages.typescript.typescriptDefaults
+      }
+      extraModules.forEach(({ content, filePath }) => {
+        monaco.editor.createModel(
+          content,
+          language === 'javascript' ? 'javascript' : 'typescript',
+          monaco.Uri.parse(filePath)
+        )
+      })
+
+      defaults.setCompilerOptions({ ...defaults.getCompilerOptions(), ...compilerOptions })
+
+      console.group('monaco detail data')
+      console.log('typescript.version', monaco.languages.typescript.typescriptVersion)
+      console.log('typescript.CompilerOptions', monaco.languages.typescript.typescriptDefaults.getCompilerOptions())
+      console.groupEnd()
+
+      return () => {
+        monaco.editor.getModels().forEach(model => {
+          if (model.uri.path !== curFilePath) model.dispose()
         })
-
-        defaults.setCompilerOptions({ ...defaults.getCompilerOptions(), ...compilerOptions })
-
-        console.group('monaco detail data')
-        console.log('typescript.version', monaco.languages.typescript.typescriptVersion)
-        console.log('typescript.CompilerOptions', monaco.languages.typescript.typescriptDefaults.getCompilerOptions())
-        console.groupEnd()
-
-        return () => {
-          monaco.editor.getModels().forEach(model => {
-            if (model.uri.path !== curFilePath) model.dispose()
-          })
-        }
-      }, [monaco, curFilePath, language, typescriptVersion])
-    },
-    topbar: [Langs],
-    statusbar: [Versions]
-  }
-  if (import.meta.hot) {
-    import.meta.hot.data['typescript:editor'] = editor
-  }
+      }
+    }, [monaco, curFilePath, language, typescriptVersion])
+  },
+  topbar: [Langs],
+  statusbar: [Versions]
 }
 
 export default definePlugin<'typescript', TypeScriptPluginX>('typescript', conf => {
