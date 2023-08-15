ca6ece8e418f6a26b96d8ffe1b1295b20e3435e8
yijie4188 <yijie4188@gmail.com>
2023-08-15 17:00:53 +0800

    chore: refactor get plugins entries logic

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,34 @@
 import react from '@vitejs/plugin-react'
 import { configDotenv } from 'dotenv'
-import fs from 'node:fs'
+import fg from 'fast-glob'
 import path from 'node:path'
 import { defineConfig } from 'vite'
 import { cdn } from 'vite-plugin-cdn2'
 import { unpkg } from 'vite-plugin-cdn2/url.js'
 
 configDotenv()
+
+const pluginEntries = fg.globSync([
+  './core/src/plugins/*.ts*',
+  './core/src/plugins/*/index.ts*',
+  '!./core/src/plugins/**/index.tsx',
+  '!./core/src/plugins/**/configure.ts',
+  './src/plugins/*.ts*',
+  './src/plugins/*/index.ts*',
+  '../ppd-plugins/*.ts*',
+  '../ppd-plugins/*/index.ts*',
+  '../ppd-plugins/*.js*',
+  '../ppd-plugins/*/index.js*'
+])
+  .reduce((acc, file) => {
+    console.log(`Adding plugin: ${file}`)
+    acc[
+      `plugins-${file.replace(/\.[jt]sx?$/, '')}`
+        .replace('/', '-')
+        .replace('.', '_')
+    ] = path.resolve(__dirname, `core/src/plugins/${file}`)
+    return acc
+  }, {} as Record<string, string>)
 
 // https://vitejs.dev/config/
 export default defineConfig(async _ => ({
@@ -30,10 +52,7 @@ export default defineConfig(async _ => ({
         main: path.resolve(__dirname, 'index.html'),
         'eval-logs': path.resolve(__dirname, 'eval-logs.html'),
         core: path.resolve(__dirname, 'core/src/index.ts'),
-        ...fs.readdirSync(path.resolve(__dirname, 'core/src/plugins')).reduce((acc, file) => {
-          acc[`plugins/${file.replace(/\.[jt]sx?$/, '')}`] = path.resolve(__dirname, `core/src/plugins/${file}`)
-          return acc
-        }, {} as Record<string, string>)
+        ...pluginEntries
       }
     }
   },
