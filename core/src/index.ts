7b06dad0ecfcabe72495100a35b500d206ede882
yijie4188 <yijie4188@gmail.com>
2023-08-19 00:13:40 +0800

    fix(core): typo

--- core/src/index.ts
+++ core/src/index.ts
@@ -2,7 +2,7 @@ export {
   default as EditorZone
 } from './components/EditorZone'
 export * from './components/QuickAccess'
-export * from './confighure'
+export * from './configure.ts'
 export * from './eval-logs/bridge'
 export * from './messenger-provider'
 export * from './plugins'
