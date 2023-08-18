5433db4973c655469abc6b8e172a200c40bf3abb
yijie4188 <yijie4188@gmail.com>
2023-08-19 01:42:12 +0800

    feat(core): better header title dev tag style

--- src/components/HeaderTitle.tsx
+++ src/components/HeaderTitle.tsx
@@ -14,8 +14,10 @@ export function HeaderTitle() {
       fontWeight: 'bolder'
     }}>P</span>layground
     {import.meta.env.MODE === 'development' && <span style={{
+      marginTop: '-1em',
       color: 'color-mix(in srgb, var(--primary), #fff 80%)',
-      fontWeight: 'bolder'
+      fontWeight: 'bolder',
+      fontSize: '0.5em'
     }}>&nbsp;(Dev)</span>}
   </>
 }
