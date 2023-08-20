74ab56e865712b17814db00544dcd61f85233ece
yijie4188 <yijie4188@gmail.com>
2023-08-20 22:27:00 +0800

    refactor(core): simplify code

--- core/src/plugins/edit-utils/index.ts
+++ core/src/plugins/edit-utils/index.ts
@@ -6,19 +6,22 @@ export default definePlugin('edit-utils', conf => ({
   editor: {
     statusbar: [GoToLC],
     load(editor, monaco) {
-      try {
-        monaco.editor.addKeybindingRule({
-          command: 'editor.action.gotoLine',
-          keybinding: monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyL
-        })
-      } catch (e) {
-        console.error(e)
-        // support lower version monaco editor
-        // TODO refactor as special version polyfill
-        editor._standaloneKeybindingService.addDynamicKeybinding('GoToLC.id', monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyL, () => {
-          editor.trigger('whatever', 'editor.action.gotoLine', {})
-        })
-      }
+      editor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyL, () => {
+        editor.trigger('whatever', 'editor.action.gotoLine', {})
+      })
+      // try {
+      //   monaco.editor.addKeybindingRule({
+      //     command: 'editor.action.gotoLine',
+      //     keybinding: monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyL
+      //   })
+      // } catch (e) {
+      //   console.error(e)
+      //   // support lower version monaco editor
+      //   // TODO refactor as special version polyfill
+      //   editor._standaloneKeybindingService.addDynamicKeybinding('GoToLC.id', monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyL, () => {
+      //     editor.trigger('whatever', 'editor.action.gotoLine', {})
+      //   })
+      // }
     }
   }
 }))
