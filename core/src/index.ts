6c1f466a0bcc0844e2e61cfd9eaaca24f647e41f
yijie4188 <yijie4188@gmail.com>
2023-08-22 23:19:27 +0800

    style(core): remove export path extension

--- core/src/index.ts
+++ core/src/index.ts
@@ -2,7 +2,7 @@ export {
   default as EditorZone
 } from './components/EditorZone'
 export * from './components/QuickAccess'
-export * from './configure.ts'
+export * from './configure'
 export * from './eval-logs/bridge'
 export * from './messenger-provider'
 export * from './plugins'
