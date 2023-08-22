02435a261a866e6b401d594383c090fc995a05f5
yijie4188 <yijie4188@gmail.com>
2023-08-23 00:38:31 +0800

    feat(core): provide more api to control localEditStateHistoryAtom in store

--- core/src/plugins/history-for-local/store.ts
+++ core/src/plugins/history-for-local/store.ts
@@ -1,15 +1,47 @@
 import { useCallback } from 'react'
-import { useAtom } from 'jotai'
+import { getDefaultStore, useAtom } from 'jotai'
 import { atomWithStorage } from 'jotai/utils'
 
 export interface EditState {
   code: string
   time: number
   lang?: string
-  cursor?: number
+  cursor?: {
+    readonly lineNumber: number;
+    readonly column: number;
+  }
 }
 
+const store = getDefaultStore()
+
 export const localEditStateHistoryAtom = atomWithStorage<EditState[]>('ppd-edit-state-history', [])
+
+export function dispatchEditState(type: 'add' | 'remove' | 'replace', editState: Omit<EditState, 'time'>, index?: number) {
+  const localEditState = store.get(localEditStateHistoryAtom)
+  if (type === 'add') {
+    store.set(localEditStateHistoryAtom, [
+      { ...editState, time: Date.now() },
+      ...localEditState
+    ])
+  }
+  if (type === 'remove') {
+    if (index === undefined) throw new Error('index is required when type is remove')
+
+    store.set(localEditStateHistoryAtom, [
+      ...localEditState.slice(0, index),
+      ...localEditState.slice(index + 1)
+    ])
+  }
+  if (type === 'replace') {
+    if (index === undefined) throw new Error('index is required when type is replace')
+
+    store.set(localEditStateHistoryAtom, [
+      ...localEditState.slice(0, index),
+      { ...editState, time: Date.now() },
+      ...localEditState.slice(index + 1)
+    ])
+  }
+}
 
 export function useLocalEditStateHistory() {
   const [history, setHistory] = useAtom(localEditStateHistoryAtom)
