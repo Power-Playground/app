cc0833fa8f37d62fd3c00308a0c9b1fdb40dbf71
yijie4188 <yijie4188@gmail.com>
2023-08-22 11:51:37 +0800

    fix(core): popover click trigger method is wrong

--- core/src/components/base/Popover.tsx
+++ core/src/components/base/Popover.tsx
@@ -70,6 +70,8 @@ export function Popover(props: PopoverProps) {
   }, [visible])
   const classname = `${prefix}-reference ${prefix}-${trigger}`
     + (props.className ? ' ' + props.className : '')
+
+  const [popoverId] = useState(Math.random().toString(36).slice(2))
   return <>
     <div
       ref={setReferenceElement}
@@ -78,7 +80,9 @@ export function Popover(props: PopoverProps) {
       onClick={() => {
         function clickOther(event: MouseEvent) {
           if (event.target instanceof HTMLElement) {
-            if (!event.target.closest(`.${prefix}`)) {
+            if (!event.target.closest(
+              `.${prefix}, .${prefix}-reference`
+            )) {
               setVisible(false)
               removeEventListener('click', clickOther)
             }
@@ -126,9 +130,9 @@ export function Popover(props: PopoverProps) {
       {content}
       <div
         ref={setArrowElement}
-        className='ppd-popover-arrow'
+        className={`${prefix}-arrow`}
         data-position={arrowPlacement}
       />
-    </div>, document.body)}
+    </div>, document.body, `popover-${popoverId}`)}
   </>
 }
