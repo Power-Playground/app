0a3741252133086a284c3b19bdc5d6421b41d456
yijie4188 <yijie4188@gmail.com>
2023-08-25 03:28:01 +0800

    feat(core): display dependency switcher CodeLens and inlay hint. (not implement)

--- core/src/plugins/typescript/index.ts
+++ core/src/plugins/typescript/index.ts
@@ -1,14 +1,19 @@
+import './index.scss'
+
 import { useEffect, useMemo } from 'react'
 import type { Editor } from '@power-playground/core'
+import { messenger } from '@power-playground/core'
 import { atom, getDefaultStore, useAtom } from 'jotai'
 import type * as monacoEditor from 'monaco-editor'
 import { mergeAll } from 'ramda'
+import sentinel from 'sentinel-js'
 
-import { definePlugin } from '../'
+import { definePlugin } from '..'
 
 import { Langs } from './statusbar/Langs'
 import { Versions } from './statusbar/Versions'
 import { use } from './use'
+import { getReferencesForModule } from './utils'
 
 export interface ExtraFile {
   content: string
@@ -48,6 +53,8 @@ export interface TypeScriptPluginX {
     changeLanguage: (lang: 'javascript' | 'typescript') => void
   }
 }
+
+const modelDecorationIdsSymbol = '_modelDecorationIds'
 
 const editor: Editor<TypeScriptPluginX> = {
   use,
@@ -101,6 +108,121 @@ const editor: Editor<TypeScriptPluginX> = {
         })
       }
     }, [monaco, curFilePath, language, defaults, extraFiles, extraModules])
+  },
+  preload(monaco) {
+    return function (disposes: (() => void)[]) {
+      return () => disposes.forEach(dispose => dispose())
+    }([
+      monaco.editor.addCommand({
+        id: 'typescript-imports',
+        run() {
+          messenger.then(m => m.display('warning', 'Switching dependency version is not supported yet'))
+        }
+      }).dispose,
+      monaco.languages.registerCodeLensProvider(['javascript', 'typescript'], {
+        provideCodeLenses(model, token) {
+          if (model.isDisposed()) return
+
+          const lenses: monacoEditor.languages.CodeLens[] = []
+          const queryRegex = /^import\s+(?:(?:\*\s+as\s+)?\w+\s+from\s+)?['"]([^'"]+)['"]$/gm
+
+          const text = model.getValue()
+          let match: RegExpExecArray | null
+
+          while ((match = queryRegex.exec(text)) !== null) {
+            const end = match.index + match[0].length - 1
+            const endPos = model.getPositionAt(end)
+            lenses.push({
+              range: new monaco.Range(endPos.lineNumber, endPos.column, endPos.lineNumber, endPos.column),
+              id: 'typescript-imports',
+              command: {
+                id: 'typescript-imports',
+                title: 'Switch @latest'
+              }
+            }, {
+              range: new monaco.Range(endPos.lineNumber, endPos.column, endPos.lineNumber, endPos.column),
+              id: 'typescript-imports',
+              command: {
+                id: 'typescript-imports',
+                title: '@beta'
+              }
+            })
+          }
+          return { lenses, dispose: () => void 0 }
+        }
+      }).dispose
+    ])
+  },
+  load(editor, monaco) {
+    const re = require as unknown as (id: string[], cb: (...args: any[]) => void) => void
+    let typescript: typeof import('typescript') | undefined = undefined
+    const decorationsCollection = editor.createDecorationsCollection()
+
+    const modelDecorationIdsConfigurableEditor = editor as unknown as {
+      [modelDecorationIdsSymbol]?: Map<string, string[]>
+    }
+    const modelDecorationIds = modelDecorationIdsConfigurableEditor[modelDecorationIdsSymbol]
+      ?? (modelDecorationIdsConfigurableEditor[modelDecorationIdsSymbol] = new Map<string, string[]>())
+    const disposable = editor.onDidChangeModelContent(function analysisCode() {
+      const model = editor.getModel()
+      if (!model) return analysisCode
+
+      const uri = model.uri.toString()
+      const ids = modelDecorationIds.get(uri)
+        ?? modelDecorationIds.set(uri, []).get(uri)!
+
+      const content = model.getValue()
+      new Promise<typeof import('typescript')>(resolve => {
+        if (typescript === undefined) {
+          re(['vs/language/typescript/tsWorker'], () => {
+            // @ts-ignore
+            typescript = window.ts
+            // @ts-ignore
+            resolve(window.ts as unknown as typeof import('typescript'))
+          })
+        } else {
+          resolve(typescript)
+        }
+      }).then(ts => {
+        const references = getReferencesForModule(ts, content)
+        editor.removeDecorations(ids)
+        const newIds = decorationsCollection.set(references.map(ref => {
+          const [start, end] = ref.position
+          const startP = model.getPositionAt(start)
+          const endP = model.getPositionAt(end)
+          const range = new monaco.Range(
+            startP.lineNumber,
+            startP.column + 1,
+            endP.lineNumber,
+            endP.column + 1
+          )
+          const inlineClassName = `ts__button-decoration ts__button-decoration__position-${start}__${end}`
+          return {
+            range,
+            options: {
+              isWholeLine: true,
+              after: {
+                content: `@${ref.version ?? 'latest'}`,
+                inlineClassName
+              }
+            }
+          } as monacoEditor.editor.IModelDeltaDecoration
+        }))
+        modelDecorationIds.set(uri, newIds)
+      })
+      return analysisCode
+    }())
+    const watchButtonDecoration = (el: HTMLDivElement) => {
+      el.addEventListener('mousedown', () => {
+        messenger.then(m => m.display('warning', 'Switching dependency version is not supported yet'))
+      })
+    }
+    sentinel.on('.ts__button-decoration', watchButtonDecoration)
+
+    return () => {
+      disposable.dispose()
+      sentinel.off('.ts__button-decoration', watchButtonDecoration)
+    }
   },
   topbar: [Langs],
   statusbar: [Versions]
