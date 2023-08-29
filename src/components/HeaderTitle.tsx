4b59acbdc712f7b018c7d9184060564e56d7429a
MicroBlock <66859419+MicroCBer@users.noreply.github.com>
2023-08-30 01:31:45 +0800

    refactor: update HeaderTitle styles

--- src/components/HeaderTitle.tsx
+++ src/components/HeaderTitle.tsx
@@ -1,15 +1,13 @@
 export function HeaderTitle() {
   return <>
-    &nbsp;
-    <span style={{
-      color: 'color-mix(in srgb, var(--primary), #fff 80%)',
-      fontWeight: 'bolder'
-    }}>P</span>ower
-    &nbsp;
-    <span style={{
-      color: 'color-mix(in srgb, var(--primary), #fff 80%)',
-      fontWeight: 'bolder'
-    }}>P</span>layground
+    <div style="
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 37px;
+"><span style="color: #fff;font-weight: bolder;text-align: justify;font-size: 22px;line-height: 10px;margin-top: 3px;">POWER</span><span style="color: color-mix(in srgb, var(--primary), #fff 80%);font-weight: 200;font-size: 16px;">Playground</span>
+</div>
+    
     {import.meta.env.MODE === 'development' && <span style={{
       marginTop: '-1em',
       color: 'color-mix(in srgb, var(--primary), #fff 80%)',
