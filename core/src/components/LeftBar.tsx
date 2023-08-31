d85e5907ecfe0c76720152ee67ee593f104adc94
yijie4188 <yijie4188@gmail.com>
2023-09-01 03:13:43 +0800

    chore: add TODO!!!

--- core/src/components/LeftBar.tsx
+++ core/src/components/LeftBar.tsx
@@ -15,6 +15,21 @@ export function LeftBar(props: LeftBarProps) {
   return <div className={classnames(prefix, props.className)}
               style={props.style}>
     <div className={`${prefix}__top`}>
+      <button onClick={() => messenger.then(m => m.display('warning', 'Not implemented yet'))}>
+        <span className='cldr codicon codicon-folder'></span>
+      </button>
+      <button onClick={() => messenger.then(m => m.display('warning', 'Not implemented yet'))}>
+        <span className='cldr codicon codicon-symbol-snippet'></span>
+      </button>
+      <button onClick={() => messenger.then(m => m.display('warning', 'Not implemented yet'))}>
+        <span className='cldr codicon codicon-source-control'></span>
+      </button>
+      <button onClick={() => messenger.then(m => m.display('warning', 'Not implemented yet'))}>
+        <span className='cldr codicon codicon-book'></span>
+      </button>
+      <button onClick={() => messenger.then(m => m.display('warning', 'Not implemented yet'))}>
+        <span className='cldr codicon codicon-extensions'></span>
+      </button>
     </div>
     <div className={`${prefix}__bottom`}>
       <button onClick={() => messenger.then(m => m.display('warning', 'Not implemented yet'))}>
