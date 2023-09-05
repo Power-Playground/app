cfbcd3d1e7d5f9523b381974e2e2149eb2e954cc
kanno <812137533@qq.com>
2023-09-05 09:42:06 +0800

    feat: add bootstrap script (#39)
    * feat: build script
    
    * feat: complete bootstrap

---
+++ scripts/bootstrap.mjs
@@ -0,0 +1,31 @@
+import child_process from 'node:child_process'
+import process from 'node:process'
+
+const defaultOptions = {
+  cwd: process.cwd(),
+  shell: process.platform === 'win32',
+  stdio: 'inherit'
+}
+
+function execa(command, argvs = [], options = defaultOptions) {
+  return new Promise((resolve, reject) => {
+    const cp = child_process.spawn(command, argvs, options)
+    cp.on('error', reject)
+    cp.on('close', (code) => {
+      if (code !== 0) {
+        const error = new Error(`Command failed with exit code ${code}`)
+        reject(error)
+      } else {
+        resolve()
+      }
+    })
+  })
+}
+
+async function main() {
+  await execa('yarn', ['install'])
+  await execa('yarn', ['build:replacer'])
+}
+
+
+main()
