2014ba005036c5ef586efe9e0b76ad1c18509937
yijie4188 <yijie4188@gmail.com>
2023-09-12 23:07:16 +0800

    feat(core): better tip style

--- core/src/components/base/HelpTip.tsx
+++ core/src/components/base/HelpTip.tsx
@@ -1,12 +1,15 @@
 import './HelpTip.scss'
 
 import type { ReactNode } from 'react'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
+
+import { classnames } from '../../utils'
 
 export type IHelpTip = [node: ReactNode, probability: number]
 
 export interface HelpTipProps {
   tips: IHelpTip[]
+  storageKey?: string
 }
 
 HelpTip.prefix = 'ppd-help-tip'
@@ -17,13 +20,21 @@ export function HelpTip(props: HelpTipProps) {
   const {
     tips
   } = props
+  const ref = useRef<HTMLDivElement>(null)
+  const [pinned, setPinned] = useState(false)
   const [helpTip, setHelpTip] = useState<IHelpTip | undefined>(getAHelpTip.bind(null, tips))
 
-  return helpTip ? <div className={prefix}>
+  return helpTip ? <div
+    ref={ref}
+    className={classnames(prefix, {
+      [`${prefix}--pinned`]: pinned
+    })}
+    >
     <span className={`${prefix}__content`}>
       {helpTip[0]}
     </span>
     <div className={`${prefix}__opts`}>
+      <div className={`${prefix}__opts__line`} />
       <span className={`${prefix}__opts__btns`}>
         <button
           title='Hide tip'
@@ -32,13 +43,23 @@ export function HelpTip(props: HelpTipProps) {
           <span className='cldr codicon codicon-close' />
         </button>
         <button
-          title='Pin tip'
-          onClick={() => setHelpTip(undefined)}
+          title={pinned ? 'Unpin tip' : 'Pin tip'}
+          onClick={() => setPinned(prev => !prev)}
         >
-          <span className='cldr codicon codicon-pin' />
+          <span className={classnames('cldr codicon', {
+            'codicon-pin': !pinned,
+            'codicon-pinned': pinned
+          })} />
         </button>
       </span>
-      <button onClick={() => setHelpTip(prev => getAHelpTip(tips, prev))}>
+      <button onClick={() => {
+        const opts = ref.current!.querySelector<HTMLDivElement>(`.${prefix}__opts__line`)!
+        opts.style.animation = 'none'
+        // noinspection BadExpressionStatementJS
+        opts.offsetHeight
+        opts.style.animation = ''
+        setHelpTip(prev => getAHelpTip(tips, prev))
+      }}>
         Next tip
         <span className='cldr codicon codicon-fold-up' />
       </button>
