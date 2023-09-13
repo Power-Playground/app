c1f970ef1a5f0c51999ebf2b26ed207f37187b59
yijie4188 <yijie4188@gmail.com>
2023-09-13 16:04:45 +0800

    feat(core): file tabs

--- core/src/plugins/mpa/index.ts
+++ core/src/plugins/mpa/index.ts
@@ -1,12 +1,14 @@
 import { definePlugin } from '..'
 
 import Project from './drawerPanels/Project'
+import { Files } from './topbar/Files'
 
 export default definePlugin({
   editor: {
     leftbar: [
       { id: Project.id, icon: 'folder' }
     ],
+    topbar: [Files],
     drawerPanels: [Project]
   }
 })
