8d1aaa25b0f6ded2eb49067997d6034ca9600644
yijie4188 <yijie4188@gmail.com>
2023-09-16 21:36:40 +0800

    feat(core): add menu for project title

--- core/src/plugins/mpa/drawerPanels/Project.tsx
+++ core/src/plugins/mpa/drawerPanels/Project.tsx
@@ -7,16 +7,39 @@ import type { ListRef } from '../../../components/base/List'
 import { List } from '../../../components/base/List'
 import type { DrawerPanelProps } from '../../../components/drawerPanelCreator'
 import { NotImplemented } from '../../../components/NotImplemented'
+import { useMenu } from '../../../hooks/useMenu'
 
 const prefix = 'ppd-drawer-panel--project'
 
 export default function Project({ template }: DrawerPanelProps) {
   const listRef = useRef<ListRef>(null)
+
+  const structureViewModeSwitchBtnRef = useRef<HTMLButtonElement>(null)
+  const {
+    popper: structureViewModeSwitchMenuPopper,
+    changeVisible: changeStructureViewModeSwitchMenuVisible
+  } = useMenu(structureViewModeSwitchBtnRef.current, [
+    {
+      id: 'project',
+      icon: 'project',
+      label: 'Project'
+    },
+    {
+      id: 'files',
+      icon: 'files',
+      label: 'Files'
+    }
+  ])
   useEffect(() => {
-    template('title', <span>
+    template('title', <button
+      ref={structureViewModeSwitchBtnRef}
+      onClick={() => changeStructureViewModeSwitchMenuVisible(true)}
+      >
       Project
       <span className='cldr codicon codicon-chevron-down' />
-    </span>)
+    </button>)
+  }, [changeStructureViewModeSwitchMenuVisible, template])
+  useEffect(() => {
     template('actions', <>
       <button onClick={() => messenger.then(m => m.display('warning', <NotImplemented />))}>
         <span className='cldr codicon codicon-expand-all' />
@@ -38,35 +61,38 @@ export default function Project({ template }: DrawerPanelProps) {
       }
     ])
   }, [template])
-  return <List
-    ref={listRef}
-    selectable
-    items={[
-      {
-        icon: 'file',
-        id: 'index.ts',
-        label: 'index.ts',
-        placeholder: '[entry]'
-      },
-      {
-        icon: 'file',
-        id: 'index.js',
-        label: 'index.js',
-        indent: 1
-      },
-      {
-        icon: 'file',
-        id: 'index.d.ts',
-        label: 'index.d.ts',
-        indent: 1
-      },
-      {
-        icon: 'folder-library',
-        id: 'node_modules',
-        label: 'node_modules',
-        className: prefix + '--dir-type__external'
-      }
-    ]}
-  />
+  return <>
+    {structureViewModeSwitchMenuPopper}
+    <List
+      ref={listRef}
+      selectable
+      items={[
+        {
+          icon: 'file',
+          id: 'index.ts',
+          label: 'index.ts',
+          placeholder: '[entry]'
+        },
+        {
+          icon: 'file',
+          id: 'index.js',
+          label: 'index.js',
+          indent: 1
+        },
+        {
+          icon: 'file',
+          id: 'index.d.ts',
+          label: 'index.d.ts',
+          indent: 1
+        },
+        {
+          icon: 'folder-library',
+          id: 'node_modules',
+          label: 'node_modules',
+          className: prefix + '--dir-type__external'
+        }
+      ]}
+    />
+  </>
 }
 Project.id = Project.icon = 'project'
