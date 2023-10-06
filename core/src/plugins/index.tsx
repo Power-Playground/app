078d40f7151d2ec725dc582228b61751a0a67658
yijie4188 <yijie4188@gmail.com>
2023-10-06 22:40:52 +0800

    fix(core): eslint error

--- core/src/plugins/index.tsx
+++ core/src/plugins/index.tsx
@@ -3,7 +3,6 @@ import type * as UITypes from '//chii/ui/legacy/legacy.js'
 import type { ReactElement } from 'react'
 import React, { createContext, useContext, useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
-import type { PrimitiveAtom } from 'jotai'
 import type * as MonacoEditor from 'monaco-editor'
 import { equals } from 'ramda'
 
@@ -71,7 +70,7 @@ export function useImports<
     }
     load()
     return () => { isMounted = false }
-  }, [get, pathsRef.current])
+  }, [get])
   return modules
 }
 
